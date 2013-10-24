9406174a2bb973b1a042da934e46073b51057718
sychu <sychu@apcode.pl>
2013-10-24 23:26:15 +0200

    empty communication package

---
+++ RC Devices/src/pl/apcode/rcdevices/communication/BluetoothLink.java
@@ -0,0 +1,19 @@
+package pl.apcode.rcdevices.communication;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class BluetoothLink extends IntentService {
+
+	public BluetoothLink(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
