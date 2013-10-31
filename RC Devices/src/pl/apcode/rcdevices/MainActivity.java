511c33883816bada7d0db072aaf0fc8ee8104cbb
sychu <sychu@apcode.pl>
2013-10-31 02:37:48 +0100

    service handling experiments

--- RC Devices/src/pl/apcode/rcdevices/MainActivity.java
+++ RC Devices/src/pl/apcode/rcdevices/MainActivity.java
@@ -68,36 +68,11 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener, O
 	private volatile float filterSensivity = 0.2f;
 	protected float[] accelVals = null;
 	
-	private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
-	      if(Build.VERSION.SDK_INT >= 10){
-	          try {
-	              final Method  m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", new Class[] { UUID.class });
-	          return (BluetoothSocket) m.invoke(device, MY_UUID);
-	          } catch (Exception e) {
-	        	  Log.e(TAG, "Could not create Insecure RFComm Connection",e);
-	          }
-	      }
-	      return  device.createRfcommSocketToServiceRecord(MY_UUID);
-	}
+
 	  
 	
 
 	
-	  private void checkBTState() {
-	    // Check for Bluetooth support and then check to make sure it is turned on
-	    // Emulator doesn't support Bluetooth and will return null
-	    if(btAdapter==null) {
-	      errorExit("Fatal Error", "Bluetooth not support");
-	    } else {
-	      if (btAdapter.isEnabled()) {
-	        Log.d(TAG, "...Bluetooth ON...");
-	      } else {
-	        //Prompt user to turn on Bluetooth
-	        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-	        startActivityForResult(enableBtIntent, 1);
-	      }
-	    }
-	  }
 	  
 	  private void errorExit(String title, String message){
 	    Toast.makeText(getBaseContext(), title + " - " + message, Toast.LENGTH_LONG).show();
@@ -131,70 +106,7 @@ public class MainActivity extends Activity implements OnSeekBarChangeListener, O
 		  return _isConnected;
 	  }
 	  
-	  private void Connect()
-	  {		    
-		    // Set up a pointer to the remote node using it's address.
-		    BluetoothDevice device = btAdapter.getRemoteDevice(DEVICE_ADDRESS);
-		    
-		    // Two things are needed to make a connection:
-		    //   A MAC address, which we got above.
-		    //   A Service ID or UUID.  In this case we are using the
-		    //     UUID for SPP.
-		    
-		    try {
-		        btSocket = createBluetoothSocket(device);
-		    } catch (IOException e1) {
-		        errorExit("Fatal Error", "In onResume() and socket create failed: " + e1.getMessage() + ".");
-		    }
-		        
-		    // Discovery is resource intensive.  Make sure it isn't going on
-		    // when you attempt to connect and pass your message.
-		    btAdapter.cancelDiscovery();
-		    
-		    // Establish the connection.  This will block until it connects.
-		    Log.d(TAG, "...Connecting...");
-		    try {
-		      btSocket.connect();
-		      Log.d(TAG, "...Connection ok...");
-		    } catch (IOException e) {
-		      try {
-		        btSocket.close();
-		      } catch (IOException e2) {
-		        errorExit("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
-		      }
-		    }
-		      
-		    // Create a data stream so we can talk to server.
-		    Log.d(TAG, "...Create Socket...");
-		  
-		    try {
-		      outStream = btSocket.getOutputStream();
-		      _isConnected = true;
-		    } catch (IOException e) {
-		      errorExit("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
-		    }	
-		    
-	  }
-	  
-	  
-	  private void Disconnect() {
-		  _isConnected = false;
-		    Log.d(TAG, "...Disconnecting...");
-		    
-		    if (outStream != null) {
-		      try {
-		        outStream.flush();
-		      } catch (IOException e) {
-		        errorExit("Fatal Error", "In onPause() and failed to flush output stream: " + e.getMessage() + ".");
-		      }
-		    }
-		  
-		    try     {
-		      btSocket.close();
-		    } catch (IOException e2) {
-		      errorExit("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
-		    }
-	  }
+
 	  
 		@Override
 		protected void onCreate(Bundle savedInstanceState) {
--- RC Devices/src/pl/apcode/rcdevices/communication/BluetoothLinkService.java
+++ RC Devices/src/pl/apcode/rcdevices/communication/BluetoothLinkService.java
@@ -1,7 +1,12 @@
 package pl.apcode.rcdevices.communication;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.ObjectOutputStream.PutField;
+import java.lang.reflect.Method;
 import java.util.UUID;
 
+import pl.apcode.rcdevices.EventLogger;
 import pl.apcode.rcdevices.R;
 
 import android.app.Notification;
@@ -9,14 +14,19 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.os.Build;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
 import android.os.Process;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 
@@ -26,36 +36,44 @@ public class BluetoothLinkService extends Service {
 	private static final UUID SSP_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 	private static final int NotificationID = 2001;
 	
+	SharedPreferences sharedPrefs;
+	
 	private NotificationManager mNotificationManager;
 	private Looper mServiceLooper;
 	private ServiceHandler mServiceHandler;
 	private Resources mRes;
+	
 	private BluetoothAdapter btAdapter = null;		
+	private BluetoothSocket btSocket = null;
+	private String deviceAdderes = "";
+	private OutputStream outStream = null;
 	
 
 	public static class MsgType 
 	{ 
-		public static final int CONNECT = 101; 
-		public static final int SEND = 102;
-		public static final int RECEIVE = 103;
-		public static final int INFO = 104;
+		public static final int CONNECT = 110;
+		public static final int SEND = 130;
+		public static final int RECEIVE = 140;
+		public static final int INFO = 150;
 	}; 
-	
 	
 
 	@Override
 	public void onCreate() {
 		Log.d(tag, "onCreate");
 		
-		HandlerThread thread = new HandlerThread("ServiceStartArguments",
+		HandlerThread thread = new HandlerThread("BluetoothLinkService",
 	            Process.THREAD_PRIORITY_DEFAULT);
 	    thread.start();
 	    
 	    mServiceLooper = thread.getLooper();
 	    mServiceHandler = new ServiceHandler(mServiceLooper);
 	    
-	    mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+	    sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+	    deviceAdderes = sharedPrefs.getString("bt-address", "");
 	    
+	    
+	    mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 	    startForeground(
 	    		NotificationID, 
 	    		getForegroundNotification(
@@ -86,74 +104,224 @@ public class BluetoothLinkService extends Service {
 	@Override
 	public void onDestroy() {
 		Log.d(tag, "onDestroy");
+		mServiceHandler.quit();
+		disconnect();
 		stopForeground(true);
-		mServiceLooper.quit();
 		super.onDestroy();
 	}
 	
 	
 	private final class ServiceHandler extends Handler {
-		  public ServiceHandler(Looper looper) {
+		
+		private boolean terminating = false;
+	    
+		
+		 public ServiceHandler(Looper looper) {
 		      super(looper);
 		      mRes = getResources(); 
 		  }
 		  
 		  
+		  private void handleConnect() {
+		  		EventLogger.d(tag, "Handle CONNECT");
+		  		removeMessages(MsgType.CONNECT);
+		  		
+				if(initBluetooth()) {
+					if(connect()) {
+						serviceNotify(
+		    				R.drawable.tx_notification_connected, 
+		    				null, 
+		    				R.string.btlink_connected);
+					} 
+					else
+					{
+						serviceNotify(
+		    				R.drawable.tx_notification_disconnected, 
+		    				null, 
+		    				R.string.btlink_unabletoconnect);
+						
+						reconnectDelayed();
+					}
+				}
+		  }
+		  
+		  
+		  private void reconnectDelayed() {
+			  postDelayed(new Runnable() {
+				public void run() {
+					EventLogger.i(tag, "Reconnecting...");
+					handleConnect();
+				}
+			}, 1000);
+		  }
+		  
+		  public void quit() {
+			 synchronized(this) { 	
+				 terminating = true;
+				 getLooper().quit();
+			 }
+		  }
+		  
+		private void serviceNotify(int iconID, Integer tickerTextID, int contentTextID) {
+			synchronized (this) {
+				if(!terminating) {
+				mNotificationManager.notify(
+			    		NotificationID, 
+			    		getForegroundNotification(
+			    				iconID, 
+			    				tickerTextID, 
+			    				contentTextID));
+				}
+			}
+		}
 		  
 		  @Override
 		  public void handleMessage(Message msg) {
 		
 			  switch(msg.what) {
 			  	case MsgType.CONNECT:
-			  		Log.d(tag, "Handle CONNECT");
-			  		initBluetooth();
+			  		handleConnect();
 			  		break;
 			  	case MsgType.SEND:
-			  		Log.d(tag, "Handle SEND");
+			  		EventLogger.d(tag, "Handle SEND");
 			  		break;
 			  	case MsgType.RECEIVE:
-			  		Log.d(tag, "Handle RECEIVE");
+			  		EventLogger.d(tag, "Handle RECEIVE");
 			  		break;
 			  	case MsgType.INFO:
-			  		Log.d(tag, "Handle INFO");
+			  		EventLogger.d(tag, "Handle INFO. " + String.valueOf(msg.obj) );
 			  		break;
 			  	default:
-			  		Log.w(tag, "Undefined handler for message: " + msg.what);
+			  		EventLogger.w(tag, "Undefined handler for message: " + msg.what);
 			  }
 			  
 		  }
+	}
+	
+	private boolean _isConnected = false;
+	public boolean IsConnected() {
+		
+		return _isConnected;
 	}
 	
 	
 	private boolean initBluetooth() {
 		boolean result = false;
 		
-		Log.d(tag, "initBluetooth");
+		EventLogger.d(tag, "initBluetooth");
 		
 		if(btAdapter == null)
 			btAdapter = BluetoothAdapter.getDefaultAdapter();
 		
 		if(btAdapter.isEnabled()) {
-			Log.d(tag,"Bluetooth is enabled");
+			EventLogger.d(tag,"Bluetooth is enabled");
 			result = true;
-			
-			mNotificationManager.notify(
-	    		NotificationID, 
-	    		getForegroundNotification(
-	    				R.drawable.tx_notification_connected, 
-	    				null, 
-	    				R.string.btlink_connected)); 
 		}
 		else {
-			Log.d(tag,"Bluetooth is disabled. Requesting enable...");
+			EventLogger.d(tag,"Bluetooth is disabled. Requesting enable...");
 			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
 			enableBtIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 			startActivity(enableBtIntent);
-			result = false;
 		}
 		return result;
 	}
 	
+	
+	private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+	      if(Build.VERSION.SDK_INT >= 10){
+	          try {
+	              final Method  m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", new Class[] { UUID.class });
+	          return (BluetoothSocket) m.invoke(device, SSP_UUID);
+	          } catch (Exception e) {
+	        	  EventLogger.e(tag, "Could not create Insecure RFComm Connection. Fallback to secure RF Cnnection...",e);
+	          }
+	      }
+	      return  device.createRfcommSocketToServiceRecord(SSP_UUID);
+	}
+	
+	
+
+	  private boolean connect()
+	  {		    
+		  disconnect();
+		  try
+		  {
+		    // Set up a pointer to the remote node using it's address.
+		    BluetoothDevice device = btAdapter.getRemoteDevice(deviceAdderes);
+		    
+		    // Two things are needed to make a connection:
+		    //   A MAC address, which we got above.
+		    //   A Service ID or UUID.  In this case we are using the
+		    //     UUID for SPP.
+		    
+		    try {
+		        btSocket = createBluetoothSocket(device);
+		    } catch (IOException e1) {
+		        EventLogger.e(tag, "Socket create failed!", e1 );
+		        return false;
+		    }
+		        
+		    // Discovery is resource intensive.  Make sure it isn't going on
+		    // when you attempt to connect and pass your message.
+		    btAdapter.cancelDiscovery();
+		    
+		    // Establish the connection.  This will block until it connects.
+		    try {
+		      btSocket.connect();
+		      EventLogger.i(tag, "Socket connection OK.");
+		    } catch (IOException e) {
+		      try {
+		    	  EventLogger.e(tag, "Socket connection FAIL!", e);
+		    	  btSocket.close();
+		        
+		      } catch (IOException e2) {
+		    	  EventLogger.e(tag, "Socket close FAIL!", e2);
+		    	  return false;
+		      }
+		      return false;
+		    }
+		      
+		    // Create a data stream so we can talk to server.		  
+		    try {
+		      outStream = btSocket.getOutputStream();
+		      
+		    } catch (IOException e) {
+		    	EventLogger.e("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
+		    }	
+		  } catch(Exception gex) {
+		    	EventLogger.e(tag, "Unable to connect device.", gex);
+		    	return false;
+		    }
+		  
+		  _isConnected = true;
+		  return true;
+	  }
+	  
+	  
+	  private void disconnect() {
+		  if(_isConnected) {
+	    	EventLogger.i(tag, "Disconnecting");
+		  
+			_isConnected = false;	    
+			  
+			if (outStream != null) {
+			  try {
+			    outStream.flush();
+			  } catch (IOException e) {
+				  EventLogger.e(tag, "Failed to flush output stream", e);
+			  }
+			}
+	
+			if(btSocket != null) {
+			    try     {
+			      btSocket.close();
+			    } catch (IOException e2) {
+			      EventLogger.e(tag, "Failed to close socket." + e2.getMessage() + ".");
+			    }
+			}
+		  }
+	  }
+	  
 	
 	Notification getForegroundNotification(int iconID, Integer tickerTextID, int contentTextID) {
 		
